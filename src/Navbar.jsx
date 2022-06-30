92e72df63340838dddc61ad8f38615cce6f9c83f
Manuel <nanoprozzi@gmail.com>
2022-06-30 20:31:21 -0300

    Implementado con switcher

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,38 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { Suspense } from 'react';
+import { Suspense , useState } from 'react';
+import i18next from 'i18next';
 
 const Navbar = () => {
 
     const { t, i18n } = useTranslation();
 
+    const [checked, setChecked] = useState(false); 
+  const handleChange = () => { 
+    
+    setChecked(!checked); 
+
+checked ? i18next.changeLanguage('es') : i18next.changeLanguage('en')
+    
+  }; 
+
+
     return (
         <nav className="navbar">
             <Link to="/"><h1>{t('navbar_title')}</h1></Link>
-            <div className="links">
-                <Link to="/">{t('navbar_home')}</Link>
-                <Link to="/contact">{t('navbar_contact')}</Link>
-                <Link to="/about">{t('navbar_about')}</Link>
-            </div>
-            <ul className='langSelector'>
-                <li></li>
-            </ul>
-        </nav>
+            <Link to="/">{t('navbar_home')}</Link>
+            <Link to="/contact">{t('navbar_contact')}</Link>
+            <Link to="/about">{t('navbar_about')}</Link>
+            <span className='switch'>
+                <input type='checkbox' id='switcher' onChange={handleChange}/>
+                <label htmlFor="switcher"></label>
+            </span>
+        </nav >
+
     );
 }
+
+
 
 export default function App() {
     return (
